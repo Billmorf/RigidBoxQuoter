13776ed537150cb590c0f3bfb324d3d0888db99f
Billmorf <bill.morfonidis@gmail.com>
2026-07-25 10:38:35 +0300

    chore: initial project setup with MVVM folder structure

---
+++ RigidBoxQuoter/RigidBoxQuoterApp.swift
@@ -0,0 +1,32 @@
+//
+//  RigidBoxQuoterApp.swift
+//  RigidBoxQuoter
+//
+//  Created by Bill Morfonidis on 25/7/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RigidBoxQuoterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
