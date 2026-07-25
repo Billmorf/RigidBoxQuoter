562f6a587a229e7c42d4f3b66c0a14d2a6b2f9c5
Billmorf <bill.morfonidis@gmail.com>
2026-07-25 14:14:58 +0300

    feat: add RawMaterial model and ViewModel with CRUD operations

---
+++ Models/MaterialUnit.swift
@@ -0,0 +1,15 @@
+//
+//  MaterialUnit.swift
+//  RigidBoxQuoter
+//
+//  Created by Bill Morfonidis on 25/7/26.
+//
+
+import Foundation
+
+enum MaterialUnit: String, Codable, CaseIterable {
+    case sheet
+    case kilogram
+    case piece
+    case squareMeter
+}
--- RigidBoxQuoter/RigidBoxQuoterApp.swift
+++ RigidBoxQuoter/RigidBoxQuoterApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct RigidBoxQuoterApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            
+            RawMaterial.self
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
