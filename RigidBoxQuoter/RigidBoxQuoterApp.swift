363b360dd12d9224e7c6dce0c1f10fab67683565
Billmorf <bill.morfonidis@gmail.com>
2026-07-27 19:26:37 +0300

    feat: complete raw material CRUD UI with add, edit, and delete

--- RigidBoxQuoter/RigidBoxQuoterApp.swift
+++ RigidBoxQuoter/RigidBoxQuoterApp.swift
@@ -25,7 +25,7 @@ struct RigidBoxQuoterApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RawMaterialListView()
         }
         .modelContainer(sharedModelContainer)
     }
